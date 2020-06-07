00f3f1c94d556012ed3a70a6c72c0bf3d48408ba
Ayush <ayush@disroot.org>
2020-06-07 21:14:39 +0530

    Initial Commit

---
+++ aosp_CPH1859.mk
@@ -0,0 +1,54 @@
+#
+# Copyright 2020 The Android Open Source Project
+# Copyright (C) 2020 The PixelExperience Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common AospExtended stuff
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Inherit from oppo CPH1861
+$(call inherit-product, device/oppo/CPH1859/device.mk)
+
+# Define first api level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Specify arch for gapps
+TARGET_GAPPS_ARCH=arm64
+
+# Bootanimation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_CPH1859
+PRODUCT_DEVICE := CPH1859
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := Realme 1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+TARGET_VENDOR_PRODUCT_NAME := CPH1859
+TARGET_VENDOR_DEVICE_NAME := CPH1859
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="CPH1859" \
+    PRODUCT_NAME="CPH1859" \
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200605.001 6392402 release-keys"
+    
+BUILD_FINGERPRINT := "google/flame/flame:10/QQ2A.200501.001.A3/6353761:user/release-keys"
